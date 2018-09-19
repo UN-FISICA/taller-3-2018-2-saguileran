fff2f3509684603b6734ec6564be8e4fc31e3388
Ricardo <ramezquitao@cihologramas.com>
2018-09-19 08:02:16 -0500

    Se adicionan archivos base del taller

---
+++ myfloat.py
@@ -0,0 +1,45 @@
+class MyFloat:
+
+    def __init__(self):
+        pass
+
+    def __add__(self):
+        pass
+
+    def __sub__(self):
+        pass
+
+    def __mul__(self):
+        pass
+
+    def __div__(self):
+        pass
+
+    def __radd__(self):
+        pass
+
+    def __rsub__(self):
+        pass
+
+    def __rmul__(self):
+        pass
+
+    def __rdiv__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self):
+        pass
+
+    def __ne__(self):
+        pass
+
+if __name__ == "__main__":
+    # Escribir aca el codigo para calcular pi. Al finalizar el calculo solo
+    # debe imprimir el valor de pi, sin otros textos ni nada
+    pass
---
+++ myfloat_func.py
@@ -0,0 +1,30 @@
+def imprimir(a):
+    return
+
+
+def suma(a, b):
+    pass
+
+
+def resta(a, b):
+    pass
+
+
+def multiplicacion(a, b):
+    pass
+
+
+def division(a, b):
+    pass
+
+
+def comparacion(a, b):
+    pass
+
+
+def pi():
+    pass
+
+
+if __name__ == "__main__":
+    print(imprimir(pi()))
